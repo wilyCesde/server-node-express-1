589a0ffb75a2d3a4276686b6cf838f9011782768
wilyCesde <wruizza@cesde.net>
2023-04-10 20:05:44 -0500

    /node_modules

--- src/index.js
+++ src/index.js
@@ -4,56 +4,58 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 2000;
 
+
+
+// Middleware para validar el usuario y la contraseña
+// function validarUsuario(req, res, next) {
+//   const { username, password } = req.body;
+//   const usuario = usuarios.find(
+//     x => x.username === username && x.password === password
+//   );
+//   if (usuario) {
+//     // Asignar un valor predeterminado de 1 (administrador) al rol del usuario
+//     usuario.role = '1';
+//     req.usuario = usuario;
+//     next();
+//   } else {
+//     res.status(401).send("Usuario o contraseña inválidos");
+//   }
+// }
+
+
+
+// function validarRol(roles) {
+//   return function (req, res, next) {
+
+//     const { role } = req.usuario;
+
+
+//     if (roles.includes(role)) {
+
+//       next();
+//     } else {
+
+//       res.status(403).send('Acceso denegado');
+//     }
+//   };
+// }
+
+// function asignarRolPredeterminado(req, res, next) {
+//   const { usuario } = req;
+//   if (usuario && !usuario.role) {
+//     usuario.role = 2;
+//   }
+//   next();
+// }
+
+// app.use(asignarRolPredeterminado);
+
 // Datos de prueba
 const usuarios = [
   { username: 'admin', fullname: 'Administrador', password: '123456', role: '1' },
   { username: 'usuario', fullname: 'Usuario', password: 'abcdef', role: '2' },
   { username: 'usuario2', fullname: 'Usuario 2', password: 'qwerty', role: '2' }
 ];
-
-// Middleware para validar el usuario y la contraseña
-function validarUsuario(req, res, next) {
-  const { username, password } = req.body;
-  const usuario = usuarios.find(
-    x => x.username === username && x.password === password
-  );
-  if (usuario) {
-    // Asignar un valor predeterminado de 1 (administrador) al rol del usuario
-    usuario.role = '1';
-    req.usuario = usuario;
-    next();
-  } else {
-    res.status(401).send("Usuario o contraseña inválidos");
-  }
-}
-
-
-
-function validarRol(roles) {
-  return function (req, res, next) {
-
-    const { role } = req.usuario;
-
-
-    if (roles.includes(role)) {
-
-      next();
-    } else {
-
-      res.status(403).send('Acceso denegado');
-    }
-  };
-}
-
-function asignarRolPredeterminado(req, res, next) {
-  const { usuario } = req;
-  if (usuario && !usuario.role) {
-    usuario.role = 2;
-  }
-  next();
-}
-
-app.use(asignarRolPredeterminado);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
